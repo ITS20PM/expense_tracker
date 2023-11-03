e46c72cfb89e7bbd42524a01c9e8b785e890ec1b
ITS20PM <patrick21meng@gmail.com>
2023-11-03 14:50:54 -0700

    work on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ const App = () => {
     <div className="App">
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg">
         <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-zinc-200 rounded">Expense Tracker</h1>
-      </div>
+
+        
       
+      </div>
     </div>
   )
 };
