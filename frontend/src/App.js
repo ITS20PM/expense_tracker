47793ac7091b2bd6ccb224f86757b7aa086f1ce8
ITS20PM <patrick21meng@gmail.com>
2023-11-03 15:09:22 -0700

    add chart feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './App.css';
+import Data from "./Components/Data";
 
 const App = () => {
   return (
     <div className="App">
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg">
-        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-zinc-200 rounded">Expense Tracker</h1>
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-zinc-200">Expense Tracker</h1>
 
-        
+        <div className="grid md:grid-cols-2 gap-6">
+          <Data></Data>
+        </div>
       
       </div>
     </div>
---
+++ frontend/src/Components/Data.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Chart} from 'chart.js';
+
+export default function 
+() {
+  return (
+    <div>
+        <div className="flex justify-content max-w-xs mx-auto">
+            <div className="item">
+                <div className="chart relative">
+                    Chart
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
