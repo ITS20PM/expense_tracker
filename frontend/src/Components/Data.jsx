f4f7233baa583de92868c00be016f3fdf3ed0f31
ITS20PM <patrick21meng@gmail.com>
2023-11-03 20:16:49 -0700

    finish Data.jsx

--- frontend/src/Components/Data.jsx
+++ frontend/src/Components/Data.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import {Chart} from 'chart.js';
+import {Chart, ArcElement} from 'chart.js';
+import {Doughnut} from 'react-chartjs-2';
 
-export default function 
-() {
+Chart.register(ArcElement);
+
+const config = {
+  data: {
+    datasets: [{
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4,
+      borderRadius: 20,
+      spacing: 5
+    }]
+  },
+
+  options: {
+    cutout: 100
+  }
+};
+
+export default function () {
   return (
     <div>
         <div className="flex justify-content max-w-xs mx-auto">
             <div className="item">
                 <div className="chart relative">
-                    Chart
+                    <Doughnut {...config}></Doughnut>
+                    <h3 className='mt-4 font-bold title'>Total
+                      <span className='block text-3xl text-green-600'>{0}</span>
+                    </h3>
                 </div>
 
+                <div className="flex flex-col py-10 gap-6"></div>
             </div>
         </div>
     </div>
