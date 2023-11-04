c423c796bce28075f332dfcfdb7ffb53775b0ef0
ITS20PM <patrick21meng@gmail.com>
2023-11-04 12:31:38 -0700

    finish form.jsx

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
+
 
 export default function Form() {
+
+    const {register, handleSubmit, resetField} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className='form max-w-sm mx-auto w-96'>
+
             <h1 className="font-bold pb-6 text-xl">Transaction</h1>  
 
-            <form id="form">
+            <form id="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid gap-4">
                     <div className="input-gp">
-                        <input type="text" placeholder="Description" className='form-input'/>
+                        <input type="text" {...register('name')} placeholder="Description" className='form-input'/>
                     </div>    
 
-                    <select className="form-input">
+                    <select className="form-input" {...register('type')}>
                         <option value="Food" defaultValue>Food</option>
-                        <option value="Transportation" defaultValue>Transportation</option>
-                        <option value="Entertainment" defaultValue>Entertainment</option>
+                        <option value="Transportation">Transportation</option>
+                        <option value="Entertainment">Entertainment</option>
                     </select>
 
                     <div className="input-gp">
-                        <input type="text" placeholder="Amount" className='form-input'/>
+                        <input type="text" {...register('amount')} placeholder="Amount" className='form-input'/>
                     </div> 
 
                     <div className="submit-btn">
